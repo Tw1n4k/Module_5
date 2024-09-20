c846e2e0c39b9b4cae516a1cdaf230dcb0fb5036
Tw1n4k <Krivonosnikolay@mail.ru>
2024-09-20 20:26:45 +0300

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -1,28 +1,26 @@
 class House:
     houses_history = []
 
-    def __init__(self, name, number_of_floors):
-        self.name = name
-        self.number_of_floors = number_of_floors
-
-    def __str__(self):
-        return self.name
-
     def __new__(cls, *args, **kwargs):
         cls.houses_history.append(args[0])
         return object.__new__(cls)
 
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
     def __del__(self):
         print(f'{self.name} снесён, но он останется в истории!')
 
+    def __str__(self):
+        return self.name
 
 
-
-h1 = House('ЖК Эльбрус', 10)
+h1 = House(input('Введите название здания'), input('Укажите количество этажей'))
 print(House.houses_history)
-h2 = House('ЖК Акация', 20)
+h2 = House(input('Введите название здания'), input('Укажите количество этажей'))
 print(House.houses_history)
-h3 = House('ЖК Матрёшки', 20)
+h3 = House(input('Введите название здания'), input('Укажите количество этажей'))
 print(House.houses_history)
 #
 # Удаление объектов
